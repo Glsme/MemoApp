c8d405951212bf398c9b6e2f2acb59fbd7a5107f
Seokjune Hong <seokjunehong@hongseogjun-ui-MacBookPro.local>
2022-05-01 21:09:35 +0900

    feat: Save button 추가

--- MemoApp/ViewController.swift
+++ MemoApp/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func save(_ sender: Any) {
+        print("Save Button Tapped!")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
