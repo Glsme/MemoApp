19a8ae1c4a1f711d2a0a86decc4947fdef6fd68c
Seokjune Hong <seokjunehong@hongseogjun-ui-MacBookPro.local>
2022-05-01 21:13:11 +0900

    feat: 폴더 및 파일 추가

---
+++ MemoApp/Model/Memo.swift
@@ -0,0 +1,20 @@
+//
+//  Memo.swift
+//  MemoApp
+//
+//  Created by Seokjune Hong on 2022/05/01.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var date: Date
+    
+    init(content: String) {
+        self.content = content
+        self.date = Date()
+    }
+    
+    static var memoList: [Memo] = []
+}
