a162c75c3191c51e9fe1def12e81ec09c2010fb3
Glsme <glasses.str.man@gmail.com>
2022-05-03 22:13:06 +0900

    chore: Alert 문구 수정

--- MemoApp/Controller/ComposeViewController.swift
+++ MemoApp/Controller/ComposeViewController.swift
@@ -8,13 +8,20 @@
 import UIKit
 
 class ComposeViewController: UIViewController {
-
+    
     @IBAction func closeButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func saveButton(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+        guard let memo = memoTextView.text, memo.count > 0 else {
+            alert(message: "메모를 입력해주세요")
+            return
+        }
+        
+        let newMemo = Memo(content: memo)
+        Memo.memoList.append(newMemo)
+//        dismiss(animated: true, completion: nil)
     }
     
     @IBOutlet weak var memoTextView: UITextView!
@@ -22,19 +29,19 @@ class ComposeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- MemoApp/Model/MemoAlert.swift
+++ MemoApp/Model/MemoAlert.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 extension UIViewController {
-    func alert (title: String = "Notice", message: String) {
-        let alert = UIAlertController(title: title, message: "test 1", preferredStyle: .alert)
+    func alert (title: String = "알림", message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(okAction)
         present(alert, animated: true, completion: nil)
