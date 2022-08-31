b7d3d95bacff68dc6361d54aaa9afdb73347556f
Glsme <glasses.str.man@gmail.com>
2022-08-31 15:29:09 +0900

    Feat: BaseViewController 파일 추가

---
+++ MemoApp/Utility/BaseViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BaseViewController.swift
+//  MemoApp
+//
+//  Created by Seokjune Hong on 2022/08/31.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    func configureUI() {
+        
+    }
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "확인", style: .default, handler: nil)
+        alert.addAction(ok)
+        present(alert, animated: true)
+    }
+}
