cfa5103c37d3d8a8037f1fb5aa5ceedefc3be61e
Glsme <glasses.str.man@gmail.com>
2022-09-01 18:37:01 +0900

    Feat: WriteView UI 구현

--- MemoApp/Write/WriteView.swift
+++ MemoApp/Write/WriteView.swift
@@ -9,5 +9,32 @@ import UIKit
 import SnapKit
 
 class WriteView: BaseView {
+    let memoTextField: UITextView = {
+        let view = UITextView()
+        
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func configureUI() {
+        self.addSubview(memoTextField)
+    }
+    
+    override func setConstraints() {
+        memoTextField.snp.makeConstraints { make in
+            make.leadingMargin.equalTo(20)
+            make.trailingMargin.equalTo(-20)
+            make.top.equalTo(self.safeAreaLayoutGuide).offset(10)
+            make.bottom.equalTo(self.safeAreaLayoutGuide).offset(-10)
+        }
+    }
     
 }
